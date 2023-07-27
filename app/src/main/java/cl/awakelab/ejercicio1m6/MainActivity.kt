92d782c5a96cab57ca0613197f8283a62da3e4b2
Fabián <fanavar@alumnos.ubiobio.cl>
2023-07-26 20:22:18 -0400

    Se agrega fragmento list y fragmento agregar. Y se configura nav_graph junto con el contenedor en el activity main

---
+++ app/src/main/java/cl/awakelab/ejercicio1m6/MainActivity.kt
@@ -0,0 +1,24 @@
+package cl.awakelab.ejercicio1m6
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/*
+[] Navigation
+[] Fragmento agregar
+[] Crear Interfaz
+[] RecyclerView
+[] Room
+    [] _Dao
+    [] _BDD
+    [] _Entity
+[] Fragmento listado
+
+
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
