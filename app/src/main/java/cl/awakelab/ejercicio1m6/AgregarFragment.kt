c6931030df7eaea9f3566a665e7ef1828a5a2cf8
Fabián <fanavar@alumnos.ubiobio.cl>
2023-07-28 19:36:53 -0400

    ejercicio 3 listo

--- app/src/main/java/cl/awakelab/ejercicio1m6/AgregarFragment.kt
+++ app/src/main/java/cl/awakelab/ejercicio1m6/AgregarFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.activityViewModels
 import cl.awakelab.ejercicio1m6.databinding.FragmentAgregarBinding
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -16,7 +18,7 @@ private const val ARG_PARAM2 = "param2"
 
 class AgregarFragment : Fragment() {
     lateinit var binding: FragmentAgregarBinding
-    lateinit var repositorio: Repositorio
+    private val tareaVM: TareaVM by activityViewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,32 +30,30 @@ class AgregarFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentAgregarBinding.inflate(layoutInflater, container, false)
-        initRepositorio()
         initListener()
         loadTareas()
         return binding.root
-    }
-    private fun initRepositorio(){
-        repositorio = Repositorio(TareaBdd.getDatabase(requireContext()).getTareaDao())
     }
 
     private fun initListener() {
         binding.btnAgregar.setOnClickListener {
             val texto = binding.etAgregar.text.toString()
             guardarTarea(texto)
+            binding.etAgregar.setText("")
+            Toast.makeText(requireContext(), "Se ha agregado un texto", Toast.LENGTH_LONG).show()
         }
     }
 
     private fun guardarTarea(texto: String) {
 
         val tarea = Tarea(texto)
-        GlobalScope.launch { repositorio.insertTarea(tarea) }
+       tareaVM.insertarTareas(tarea)
     }
 
     private fun loadTareas() {
 
 
-            repositorio.getTareas().observe(requireActivity()){
+           tareaVM.obtenerTareas().observe(viewLifecycleOwner){
 
                 val tareaAsText = it.joinToString("\n") { it.nombre }
                 binding.tvAgregar.text = tareaAsText
